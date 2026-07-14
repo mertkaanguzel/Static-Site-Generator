c0410d4dda5a122eb749b4972bbf133133e9f102
Mert Kaan Güzel <84642031+mertkaanguzel@users.noreply.github.com>
2026-07-14 22:29:10 +0000

    Implement TextType enum and TextNode class

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main() -> None:
+    textnode = TextNode("This is some anchor text", TextType.LINK_TEXT, "https://www.boot.dev")
+    print(textnode)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN_TEXT = "text"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK_TEXT = "link"
+    IMAGE_TEXT = "image"
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TextNode):
+            return False
+        
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+    
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
